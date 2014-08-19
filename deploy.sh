ec5a5022248011d35e1d452831bfef543f21691f
Erik Schwartz <erik@erikschwartz.net>
2014-08-19 14:55:09 -0400

    Only deploy if clean working dir

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
-git pull origin gh-pages
-cp -a public/* .
-git add .
-git commit -m 'build'
-git push origin gh-pages
+if [[ $(git diff --shortstat HEAD 2> /dev/null | tail -n1) != "" ]]; then
+  echo 'working directory is dirty, please commit all changes before deploy'
+else
+  git pull origin gh-pages
+  cp -a public/* .
+  git add .
+  git commit -m 'build'
+  git push origin gh-pages
+fi

e5676fa8db46d10be8698c28d770bc316b6d7b9f
Erik Schwartz <erik@erikschwartz.net>
2014-08-25 20:49:44 -0400

    Clean working dir before copying public

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@ if [[ $(git status --short) != "" ]]; then
   echo 'working directory is dirty, please commit all changes before deploy'
 else
   git pull origin gh-pages
+  # start with a clean slate and copy from public so that files removed
+  # from the build are removed from gh-pages
+  ls | grep -v bower | grep -v deploy | grep -v node_modules | grep -v public | xargs git rm -r
   cp -a public/* .
   git add .
   git commit -m 'build'

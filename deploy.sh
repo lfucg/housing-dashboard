fe55535df3ecb9bf7ceeea088a1a984dabcfbf30
Erik Schwartz <erik@erikschwartz.net>
2014-11-03 12:49:24 -0800

    Deploy _site instead of public

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@ if [[ $(git status --short) != "" ]]; then
   echo 'working directory is dirty, please commit all changes before deploy'
 else
   git pull origin gh-pages
-  # start with a clean slate and copy from public so that files removed
+  # start with a clean slate and copy from _site so that files removed
   # from the build are removed from gh-pages
-  ls | grep -v bower | grep -v deploy | grep -v node_modules | grep -v public | xargs git rm -r
-  cp -a public/* .
+  ls | grep -v bower | grep -v deploy | grep -v node_modules | grep -v _site | xargs git rm -r
+  cp -a _site/* .
   git add .
   git commit -m 'build'
   git push origin gh-pages
